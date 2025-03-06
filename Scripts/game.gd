73a27f091e4512ce991362a0e92f639d2a7d328d
SixFours <199505686+SixFours@users.noreply.github.com>
2025-03-06 16:51:15 -0500

    Added Dirt Clumps around the map

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -3,3 +3,10 @@ extends Node2D
 #func _process(delta: float) -> void:
 	#if $Music.fini == true:
 		#$Music.play()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):  # "ui_cancel" is mapped to the ESC key by default
+		change_scene("res://Scenes/esc.menu.tscn")  # Replace with your scene path
+
+func change_scene(scene_path: String):
+	get_tree().change_scene()  # Change to the specified scene
